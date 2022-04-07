c9681a03ce438046b44da66eade41ff66e44e4eb
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-04-07 15:39:14 -0300

    projeto teste de angular - entendendo conceitos

---
+++ src/app/components/parents-data/parents-data.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ParentsDataComponent } from './parents-data.component';
+
+describe('ParentsDataComponent', () => {
+  let component: ParentsDataComponent;
+  let fixture: ComponentFixture<ParentsDataComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ParentsDataComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ParentsDataComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
